ca6d033d1d76d066a1047a873456ce0d62d68a98
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2021-04-16 09:55:16 -0300

    Projeto Concluido

---
+++ Fixacao/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fixacao.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
